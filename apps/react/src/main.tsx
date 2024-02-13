2d58e7e42627e43f7eb4fede35f123a3f67eddc6
Patricio Beckmann <pato.beckmann@gmail.com>
2024-02-13 15:43:26 -0300

    feat: full streaming

--- apps/react/src/main.tsx
+++ apps/react/src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM, { createRoot } from 'react-dom/client'
 import { App } from './App.tsx'
 import { BrighttiStreamingProvider } from '@brightti/react'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.body).render(
   <React.StrictMode>
     <BrighttiStreamingProvider config={{
       AutoConnect: true, // autoconect to queue and then streaming, default: false
@@ -12,7 +12,8 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       AutoConnectStreaming: true, // autoconnect to streaming when queue finish. (if AutoConnect its set to true, AutoConnectStreaming not take effect). default: false
       AutoPlayVideo: true, // When video is ready automatically start playing (some browsers fails whitout interaction, use StartVideoMuted true to autoplay whitout human interaction), default: true
       StartVideoMuted: true, // start the video without audio, default: false
-      HoveringMouse: false, // Either locked mouse, where the pointer is consumed by the video and locked to it, or hovering mouse, where the mouse is not consumed. default: false
+      HoveringMouse: true, // Either locked mouse, where the pointer is consumed by the video and locked to it, or hovering mouse, where the mouse is not consumed. default: false
+      MatchViewportRes: true // Scale the streaming to match Viewport, default: false
     }}>
       <App />
     </BrighttiStreamingProvider>
